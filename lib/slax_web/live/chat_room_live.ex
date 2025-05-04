f9018f2a21391961cdb3d00a3684733ab59d05ea
avidang01~ <avidan627@gmail.com>
2025-05-04 20:42:04 +0300

    feat: enhance chat room display with topic toggle functionality

--- lib/slax_web/live/chat_room_live.ex
+++ lib/slax_web/live/chat_room_live.ex
@@ -7,7 +7,10 @@ defmodule SlaxWeb.ChatRoomLive do
   def mount(_params, _session, socket) do
     room = Room |> Repo.all() |> List.first()
 
-    {:ok, assign(socket, room: room)}
+    {:ok,
+     socket
+     |> assign(:room, room)
+     |> assign(hide_topic?: false)}
   end
 
   def render(assigns) do
@@ -16,15 +19,25 @@ defmodule SlaxWeb.ChatRoomLive do
       <div class="flex justify-between items-center shrink-0 h-16 bg-white border-b border-slate-300 px-4">
         <div class="flex flex-col gap-1.5">
           <h1 class="text-sm font-bold leading-none">
-          #{@room.name}
+            #{@room.name}
           </h1>
-
-          <div class="text-xs leading-none h-3.5">
-           {@room.topic}
+          <div
+            class={["text-xs leading-none h-3.5 cursor-default select-none", @hide_topic? && "text-slate-600"]}
+            phx-click="toggle_topic"
+          >
+            <%= if @hide_topic? do %>
+              [Topic hidden]
+            <% else %>
+              {@room.topic}
+            <% end %>
           </div>
         </div>
       </div>
     </div>
     """
+  end
+
+  def handle_event("toggle_topic", _params, socket) do
+    {:noreply, socket |> assign(:hide_topic?, !socket.assigns.hide_topic?)}
   end
 end
